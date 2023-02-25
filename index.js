7dc4d518cb3607539e5a071fe448af64fa8b5e9a
Michael Patrick <michael.patrick.org@gmail.com>
2023-02-24 18:55:22 -0800

    added display to the on board to the note keep

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ var noteArray = [];
 
 function addNotes() {
     let item = document.getElementById("keepNote").value;
-    let noteObj = {item: item, Date: new Date().toUTCString()}
+    let noteObj = {item: item, Date: new Date().toUTCString()};
+
     noteArray.push(noteObj);
 
     displayNote();
 }
 
 function displayNote() {
-    for(i = 0; i < noteArray.length; i++)
-    console.log(noteArray[i])
+    let content = "";
+    for(i = 0; i < noteArray.length; i++){
+        content += "<p>" + noteArray[i].item + "</p>"
+    }
+
+    document.getElementById("showBoard").innerHTML = content;
 }
