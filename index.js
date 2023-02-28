ee086a5bfa5e171ed88e9bf6b88db9fc4ad68b1c
Michael Patrick <michael.patrick.org@gmail.com>
2023-02-27 19:03:22 -0800

    added local storage to the keep note

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 
 var noteArray = [];
 
+function getNote(){
+    let localNote = localStorage.getItem('notes');
+    if(localNote){
+        noteArray = JSON.parse(localNote);
+    }else{
+        noteArray = [];
+    }
+    
+    displayNote();
+}
+
 function addNotes() {
     let item = document.getElementById("keepNote").value;
     let noteObj = {item: item, Date: new Date().toUTCString()};
 
     noteArray.push(noteObj);
 
+    saveNote();
     displayNote();
+}
+
+function saveNote(){
+    localStorage.setItem('notes', JSON.stringify(noteArray));
 }
 
 function displayNote() {
     let content = "";
     for(i = 0; i < noteArray.length; i++){
-        content += "<p>" + noteArray[i].item + "</p>"
+        content += "<p>" + noteArray[i].item + "</p>" + "<hr>"
     }
-
+    
     document.getElementById("showBoard").innerHTML = content;
 }
